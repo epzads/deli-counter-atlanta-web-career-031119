b7a7a238b4b4692f6d340a5c744d602087437b47
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 14:18:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,21 @@ def line(array = [] )
   if array == []
  puts "The line is currently empty."  
 else 
+size = array.size
+
+#puts size 
+count = 1 
+numberarray = []
+size.times do |h|
+numberarray << count.to_s + ". " +  array[h] 
+
+count += 1 
+end 
+puts numberarray
+
+
+
+puts "The line is currently : #{numberarray.join(" ")}"
 
 
 end 
